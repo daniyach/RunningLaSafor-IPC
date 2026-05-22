8b8ac7864d08166ae39e2ab951e12ac153977de6
fabio <fabio@FABIO>
2026-05-22 17:30:47 +0200

    feat: Pantalla de Registro (falta la programación)

--- src/register/FXMLController.java
+++ src/register/FXMLController.java
@@ -2,11 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package login;
+package register;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,12 +18,17 @@ import javafx.fxml.Initializable;
  */
 public class FXMLController implements Initializable {
 
+    @FXML
+    private ImageView login_foto;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Image imagenLocal = new Image(getClass().getResourceAsStream("/resources/logologin.jpg"));
+        login_foto.setImage(imagenLocal);
     }    
     
 }
