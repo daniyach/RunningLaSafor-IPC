ebac2ff8642aad34f510b188f4d0fabb7e891d48
fabio <fabio@FABIO>
2026-05-22 17:52:11 +0200

    feat: Pantalla de Login (falta la programación)

--- src/login/FXMLController.java
+++ src/login/FXMLController.java
@@ -6,7 +6,14 @@ package login;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -15,6 +22,17 @@ import javafx.fxml.Initializable;
  */
 public class FXMLController implements Initializable {
 
+    @FXML
+    private ImageView foto_iniciarsesion;
+    @FXML
+    private TextField nickname_login;
+    @FXML
+    private PasswordField contraseña_login;
+    @FXML
+    private Text crear_cuenta;
+    @FXML
+    private Button iniciar_login;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +40,10 @@ public class FXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleCrear(MouseEvent event) {
+        
+    }
     
 }
